572fd4855ed3e077214fb84e22ccfcd672ebbfc9
ayelet <ayelet47654@gmail.com>
2021-01-16 13:09:07 +0200

    hjkhkj

--- game.js
+++ game.js
@@ -1,61 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 let cardArray = [ 
-    { name: "pic1", img: "images/pic1.png", }, 
-    { name: "pic1", img: "images/pic1.png", }, 
-    { name: "pic2", img: "images/pic2.png", },
-    { name: "pic2", img: "images/pic2.png", },
-    { name: "pic3", img: "images/pic3.png", },
-    { name: "pic3", img: "images/pic3.png", },
-    { name: "pic4", img: "images/pic4.png", },
-    { name: "pic4", img: "images/pic4.png", }, 
-    { name: "pic5", img: "images/pic5.png", },
-    { name: "pic5", img: "images/pic5.png", },
-    { name: "pic6", img: "images/pic6.png", }, 
-    { name: "pic6", img: "images/pic6.png", }, 
-    { name: "pic7", img: "images/pic7.png", },
-    { name: "pic7", img: "images/pic7.png", },
-    { name: "pic8", img: "images/pic8.png", },
-    { name: "pic8", img: "images/pic8.png", },
-    { name: "pic9", img: "images/pic9.png", },
-    { name: "pic9", img: "images/pic9.png", },
-    { name: "pic10", img: "images/pic10.png", },
-    { name: "pic10", img: "images/pic10.png", },
-    { name: "pic11", img: "images/pic11.png", },
-    { name: "pic11", img: "images/pic11.png", }, 
-    { name: "pic12", img: "images/pic12.png", },
-    { name: "pic12", img: "images/pic12.png", },
-    { name: "pic13", img: "images/pic13.png", },
-    { name: "pic13", img: "images/pic13.png", },
-    { name: "pic14", img: "images/pic14.png", },
-    { name: "pic14", img: "images/pic14.png", },
-    { name: "pic15", img: "images/pic15.png", },
-    { name: "pic15", img: "images/pic15.png", },
+    { name: "pic1", img: "images/pic1.jpg", }, 
+    { name: "pic1", img: "images/pic1.jpg", }, 
+    { name: "pic2", img: "images/pic2.jpg", },
+    { name: "pic2", img: "images/pic2.jpg", },
+    { name: "pic3", img: "images/pic3.jpg", },
+    { name: "pic3", img: "images/pic3.jpg", },
+    { name: "pic4", img: "images/pic4.jpg", },
+    { name: "pic4", img: "images/pic4.jpg", }, 
+    { name: "pic5", img: "images/pic5.jpg", },
+    { name: "pic5", img: "images/pic5.jpg", },
+    { name: "pic6", img: "images/pic6.jpg", }, 
+    { name: "pic6", img: "images/pic6.jpg", }, 
+    { name: "pic7", img: "images/pic7.jpg", },
+    { name: "pic7", img: "images/pic7.jpg", },
+    { name: "pic8", img: "images/pic8.jpg", },
+    { name: "pic8", img: "images/pic8.jpg", },
+    { name: "pic9", img: "images/pic9.jpg", },
+    { name: "pic9", img: "images/pic9.jpg", },
+    { name: "pic10", img: "images/pic10.jpg", },
+    { name: "pic10", img: "images/pic10.jpg", },
+    { name: "pic11", img: "images/pic11.jpg", },
+    { name: "pic11", img: "images/pic11.jpg", }, 
+    { name: "pic12", img: "images/pic12.jpg", },
+    { name: "pic12", img: "images/pic12.jpg", },
+    { name: "pic13", img: "images/pic13.jpg", },
+    { name: "pic13", img: "images/pic13.jpg", },
+    { name: "pic14", img: "images/pic14.jpg", },
+    { name: "pic14", img: "images/pic14.jpg", },
+    { name: "pic15", img: "images/pic15.jpg", },
+    { name: "pic15", img: "images/pic15.jpg", },
     ]; 
-const numOfRows=5;
-const numOfColumns=6;
-var cardsMatched = 0;
-var cardsFlipped = 0; 
+const numOfRows = 5;
+const numOfColumns = 6;
+let cardsMatched = 0;
+let cardsFlipped = 0; 
+let scoreDisplay = $('#scoreNum')
+let movesDisplay= $('#movesNum')
 let deck = $('#deck')
 
 function loadGame(){
-    $("re-btn").click(createBoard);
+    $("#re-btn").click(restartGame);
+    shuffle(cardArray);
     createBoard();
 }
 
   //create your board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-        let card = document.createElement('img');
-        card.setAttribute('src', 'images/back.jpg');
-        card.setAttribute('data-id', i);
-        card.className='card';
-        //card.addEventListener('click', flipCard);
+        let card = $('<img />', { 
+            id: ''+i,
+            src: "images/back.jpg",
+            class: 'card card-back',
+            alt: 'MyAlt'
+        });
+        card.click(flipCard);
         deck.append(card);
     }
 }
 
- 
+function restartGame(){
+    alert("restart");
+}
+
+function flipCard(event){
+    let index = event.target.id;
+    $('#'+index).attr("src", cardArray[index].img);
+    let name = a
+    if()
+}
+
 // Fisher-Yates (aka Knuth) Shuffle
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -69,6 +84,6 @@ function shuffle(array) {
     return array;
 }
 
-
 loadGame();
+
 });
